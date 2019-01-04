1724d310e156dddb4205bb7ebc24194fcca79267
mathix <mathixx@gmail.com>
2019-01-04 18:21:38 +0100

    Fetching package info

--- commands/install_command.py
+++ commands/install_command.py
@@ -1,3 +1,9 @@
+import requests
+
 def install_command(args):
     package = args.package
     print(f'installing package {package}')
+
+    with requests.Session() as session:
+        response = session.get(f'http://localhost:7001/package-info/test.packageA', headers={'Zippero-Api-Key': 'xxx'})
+        print(response.json())
