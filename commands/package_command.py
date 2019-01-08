f7ff484244019e1e6663a74c3adb0d2d875c3f27
mathix <mathixx@gmail.com>
2019-01-08 17:57:31 +0100

    Package command

--- commands/package_command.py
+++ commands/package_command.py
@@ -1,3 +1,30 @@
+import shutil
+import sys
+from os import path
+
+from utils.paths import zpspec_name
+from utils.zpspec_utils import load_zpspec, fullname
+
+
 def package_command(args):
     print('package command')
     print(args)
+
+    directory = args.directory
+    zpspec_path = path.join(directory, zpspec_name)
+
+    zpspec_dict = load_zpsec_or_handle_err(zpspec_path)
+    name = zpspec_dict['packageName']
+    version = zpspec_dict['version']
+
+    archive_path = path.join(directory, fullname(name, version) + '.zip')
+
+    shutil.make_archive(archive_path, 'zip', directory)
+
+
+def load_zpsec_or_handle_err(zpspec_path):
+    try:
+        return load_zpspec(zpspec_path)
+    except OSError as ex:
+        print(f'Directory {directory} does not contain {zpspec_name}', file=sys.stderr)
+        raise
---
+++ utils/zpspec_utils.py
@@ -0,0 +1,18 @@
+import json
+
+
+def load_zpspec(zpspec_path: str):
+    with open(zpspec_path, 'rt') as file:
+        return json.load(zpspec_path, file)
+
+
+def fullname(name: str, version: str):
+    return f'{name}@{version}'
+
+
+def parse_fullname(fullname: str):
+    if not '@' in fullname:
+        raise ValueError(f'"{fullname}" is not proper package fullname')
+
+    [name, version] = fullname.split('@')
+    return (name, version)
