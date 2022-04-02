b64a142639787674977622d2c62a8516c93c932e
Jan <jan.rycko@gmail.com>
2022-04-03 01:07:52 +0200

    package_versions_command.

--- packages/package_utils.py
+++ packages/package_utils.py
@@ -14,3 +14,14 @@ def get_newest_package_from_package_info(package_info: Dict):
 
     newest_version = versions[-1]
     return newest_version
+
+
+def get_package_versions(package_info: Dict):
+    versions = package_info['data']['versions'].copy()
+
+    if len(versions) == 0:
+        raise ZipperoClientException(f'No versions of {zpspec_name} found')
+
+    versions.sort(key=LooseVersion)
+
+    return versions
